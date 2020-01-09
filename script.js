213bdd0121cdb104465061aa3c19ef0782dade7b
Raja <singhrb911@gmail.com>
2020-01-10 02:44:27 +0530

    added smooth scroll

--- script.js
+++ script.js
@@ -1,12 +1,52 @@
-$(document).ready(function(){
-$(".nav-link").click(function (e){
+$(document).ready(function () {
+    $("a.nav-link").on('click', function (event) {
 
-    e.preventDefault();
-    target = $(this.hash).offset().top;
-    $("html, body").animate({
-        scrollTop: target
-    }, 800);
-   
-});
+        if (this.hash !== "") {
+            // Prevent default anchor click behavior
+            event.preventDefault();
+            var hash = this.hash;
+
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 2000, function () {
+                window.location.hash = hash;
+            });
+        } // End if
+    });
+    //    for back2top scroll
+    $(window).scroll(function () {
+        var height = $(window).scrollTop();
+        if (height > 100) {
+            $('#back2Top').fadeIn();
+        } else {
+            $('#back2Top').fadeOut();
+        }
+    });
+    $("#back2Top").click(function (event) {
+        event.preventDefault();
+        $("html, body").animate({
+            scrollTop: 0
+        }, 2000);
+        return false;
+    }); //for scroll2top end
+
 });
 
+//$(window).scroll(function () {
+//    var height = $(window).scrollTop();
+//    if (height > 100) {
+//        $('#back2Top').fadeIn();
+//    } else {
+//        $('#back2Top').fadeOut();
+//    }
+//});
+//$(document).ready(function () {
+//    $("#back2Top").click(function (event) {
+//        event.preventDefault();
+//        $("html, body").animate({
+//            scrollTop: 0
+//        }, 2000);
+//        return false;
+//    });
+//
+//});
