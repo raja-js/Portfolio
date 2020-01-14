73814c18c27da0b6db50e97772e2c735112b90a1
Raja <singhrb911@gmail.com>
2020-01-14 23:25:49 +0530

    edited footer and some section

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 $(document).ready(function () {
+    $('html, body').scrollTop(0);
+
+    $(window).on('load', function() {
+    setTimeout(function(){
+        $('html, body').scrollTop(0);
+    }, 0);
+ });
     $("a.nav-link").on('click', function (event) {
 
         if (this.hash !== "") {
